12afa031768a167e8d9c34d9a11f838c502878c2
Evgeniya Okuneva <evgeniyaokuneva85@gemail.com>
2023-04-27 16:56:04 +0400

    2 вариант

--- task_payton.py
+++ task_payton.py
@@ -1,8 +1,28 @@
 import uuid
 
-
+# 1 вариант
 def test_uuid():
-    id_str = uuid.uuid4()
-    id_str1 = str(uuid.uuid4())
-    id_str1 = 'autotest' + id_str1[8:]
-    print(id_str1)
+    id_str = str(uuid.uuid4())
+    id_str1 = 'autotest' + id_str[8:]
+    return (id_str1)
+
+
+# 2 вариант
+
+def test_uuid1():
+    id_str = str(uuid.uuid4())
+    li = id_str.split('-')
+    li[0] = 'autotest'
+    return ('-'.join(li))
+
+#3 вариант
+def test_uuid2():
+    id_str = str(uuid.uuid4())
+    li1 = id_str.split('-')
+    for i in range (len(li1)):
+      if i==0:
+         li1[i] = 'autotest'
+
+    return ('-'.join(li1))
+
+
